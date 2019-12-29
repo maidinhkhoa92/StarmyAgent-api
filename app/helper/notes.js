3be1b8eed4ac942550a4b5238e996a19e6dc3fea
maidinhkhoa92 <maidinhkhoa92@gmail.com>
2019-12-30 00:08:18 +0700

    init source base

--- app/helper/notes.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    {description: 'L@ llevo a las'},
-    {description: 'L@ recojo a las'},
-    {description: 'L@ recogerá'},
-    {description: 'Le toca la medicina a las'},
-    {description: 'No l@ llevo hoy'}
-]
