d3a38136c305faf1c45362609fbccdeed5157976
danhmd97 <danhmd1997@gmail.com>
2020-03-10 20:44:46 +0700

    Revert "test"
    This reverts commit 33b320155884f821e756bb10beb30a9885e661c3.

--- app/back-end/route.js
+++ app/back-end/route.js
@@ -2,9 +2,4 @@ const express = require('express');
 const router = express.Router();
 const Token = require('../helper/token');
 
-// user
-const Test = require("./test");
-
-router.post("/test", Test.create);
-
 module.exports = router;
